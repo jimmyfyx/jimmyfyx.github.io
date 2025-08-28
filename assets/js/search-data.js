902c1b71fdfae887f9525f61f0158aa22bc43f73
jimmyfyx <99617468+jimmyfyx@users.noreply.github.com>
2025-08-28 23:49:05 +0000

    Deploying to gh-pages from @ jimmyfyx/jimmyfyx.github.io@dd4d060ba5ee4a33e5f61079e9ee364529ee36f9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of your cool projects.",
+          description: "A growing collection of my side projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -429,49 +429,14 @@ ninja.data = [{
           section: "News",},{id: "news-started-my-internship-on-3d-sensor-simulation-at-zoox-inc",
           title: 'Started my internship on 3D sensor simulation at Zoox Inc.!',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-agricultural-robots-simulation-environment",
+          title: 'Agricultural Robots Simulation Environment',
+          description: "Build a simulation environment for under-canopy agricultural field robots",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
           title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
+          description: "More projects to be updated ...",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
